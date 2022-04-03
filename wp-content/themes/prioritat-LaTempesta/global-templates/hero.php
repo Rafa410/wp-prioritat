77e913b6bc8945d8785eaa3bf15e9817699d33c1
Rafa Soler <rafasoler10@gmail.com>
2022-04-04 01:15:32 +0200

    Add hero

---
+++ wp-content/themes/prioritat-LaTempesta/global-templates/hero.php
@@ -0,0 +1,12 @@
+
+<div class="homepage-hero d-flex align-items-center min-vh-100 bg-primar">
+    <div class="bg-image position-absolute">
+        <?php the_post_thumbnail(); ?>
+    </div>
+    <div class="hero-content d-flex flex-column align-items-start text-light">
+        <h1 class="bg-title pe-2">Priorat</h1>
+        <h2 class="bg-title pe-3">Paisatge cultural patrimoni mundial</h2>
+        <p class="description my-3 p-3">Super breu explicació de què és l'asociació, què pretén i pq algú s'hauria d'unir. Lorem ipsum dolor sit amet, consetetur sadipscing elits, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <a href="/fes-te-soci" class="btn btn-lg btn-secondary btn-animated position-relative py-1 text-light fw-bold"><span>Fes-te soci</span></a>
+    </div>
+</div>
