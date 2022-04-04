408f6c85a307aac1135b119faeafb379a60b6133
Rafa Soler <rafasoler10@gmail.com>
2022-04-05 00:11:16 +0200

    Add Values section

--- wp-content/themes/prioritat-LaTempesta/page-templates/homepage-template.php
+++ wp-content/themes/prioritat-LaTempesta/page-templates/homepage-template.php
@@ -28,7 +28,61 @@ if ( is_front_page() ) {
 
 				<main class="site-main" id="main" role="main">
 
-					<section></section>
+					<section id="values">
+
+						<div class="values-list d-flex flex-wrap gap-3 justify-content-center">
+
+							<div class="value p-3" id="mermership">
+								
+								<header>
+									<h3>Pertinença</h3>
+								</header>
+								<p class="description lh-sm">
+									Sense persones no hi ha paisatge. El paisatge del Priorat és un mosaic divers i ric, ple de contrastos derivats de la interacció dels seus habitants amb el territori. La disposició del paisatge ens mostra el transcurs de la història humana, diferenciant espais forestals, urbans i agrícoles.
+								</p>
+								<a href="#" class="read-more">Més informació</a>
+
+							</div>
+
+							<div class="value p-3" id="harmony">
+								
+								<header>
+									<h3>Harmonia del paisatge</h3>
+								</header>
+								<p class="description lh-sm">
+									L'harmonia és una característica del mosaic paisatgístic del Priorat. Els pobles s'integren plenament a l'orografia del territori, connectats per una xarxa de camins densa, rica i ben conservada. El paisatge engloba els espais naturals, cultius tradicionals, patrimoni històric i arquitectònic.
+								</p>
+								<a href="#" class="read-more">Més informació</a>
+
+							</div>
+
+							<div class="value p-3" id="culture">
+								
+								<header>
+									<h3>Cultura natural</h3>
+								</header>
+								<p class="description lh-sm">
+									La vida socioeconòmica es troba plenament integrada a l'entorn, ric i divers en flora i fauna, donant lloc a una particular cultura indissociable del medi. Tot plegat estableix un lligam de gran trascendència històrica entre cultura i natura, donant lloc a un equilibri natural, humà i social que conforma la nostra identitat.
+								</p>
+								<a href="#" class="read-more">Més informació</a>
+
+							</div>
+
+							<div class="value p-3" id="heritage">
+								
+								<header>
+									<h3>Patrimoni immaterial</h3>
+								</header>
+								<p class="description lh-sm">
+									Sense persones no hi ha paisatge. El paisatge del Priorat és un mosaic divers i ric, ple de contrastos derivats de la interacció dels seus habitants amb el territori. La disposició del paisatge ens mostra el transcurs de la història humana, diferenciant espais forestals, urbans i agrícoles.
+								</p>
+								<a href="#" class="read-more">Més informació</a>
+
+							</div>
+
+						</div>
+
+					</section>
 
 				</main><!-- #main -->
 
