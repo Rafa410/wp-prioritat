24534d3fd441de84a7c18b2621d4f521f6679f7a
Rafa Soler <rafasoler10@gmail.com>
2022-04-07 00:53:52 +0200

    Agenda section (UI)

--- wp-content/themes/prioritat-LaTempesta/page-templates/homepage-template.php
+++ wp-content/themes/prioritat-LaTempesta/page-templates/homepage-template.php
@@ -84,6 +84,57 @@ if ( is_front_page() ) {
 
 					</section>
 
+					<section id="agenda" class="my-5">
+
+						<header>
+							<h2 class="bg-title bg-title-tertiary">Agenda</h2>
+						</header>
+
+						<div class="row g-0 align-items-center">
+
+							<div class="col-md-5 p-3">
+								<div class="calendar-monthly-view"></div>
+							</div>
+
+							<div class="col-md-7 ps-4">
+								<div class="calendar-list-view">
+									<div class="calendar-event d-flex my-3">
+										<div class="event-date p-3 bg-primary">
+											<span class="day">06</span>
+											<span class="month">abril</span>
+										</div>
+										<div class="event-content p-2">
+											<h3 class="title">Títol de l'activitat, lorem ipsum dolor sit amet, consetetur sadipscing</h3>
+											<p class="description">Informació sobre l'activitat, super breu lorem ipsum dolor sit amet, consetetur sadipscing</p>
+										</div>
+									</div>
+									<div class="calendar-event d-flex my-3">
+										<div class="event-date p-3 bg-primary">
+											<span class="day">15</span>
+											<span class="month">abril</span>
+										</div>
+										<div class="event-content p-2">
+											<h3 class="title">Títol de l'activitat, lorem ipsum dolor sit amet, consetetur sadipscing</h3>
+											<p class="description">Informació sobre l'activitat, super breu lorem ipsum dolor sit amet, consetetur sadipscing</p>
+										</div>
+									</div>
+									<div class="calendar-event d-flex my-3">
+										<div class="event-date p-3 bg-secondary">
+											<span class="day">23</span>
+											<span class="month">abril</span>
+										</div>
+										<div class="event-content p-2">
+											<h3 class="title">Títol de l'activitat, lorem ipsum dolor sit amet, consetetur sadipscing</h3>
+											<p class="description">Informació sobre l'activitat, super breu lorem ipsum dolor sit amet, consetetur sadipscing</p>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+					</section>
+
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
