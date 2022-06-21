e701980f2488fe9af03974c854d3eef73c8cafe8
Rafa Soler <rafasoler10@gmail.com>
2022-06-22 00:31:30 +0200

    Pause media when closing modal

--- wp-content/themes/prioritat-LaTempesta/src/js/custom-javascript.js
+++ wp-content/themes/prioritat-LaTempesta/src/js/custom-javascript.js
@@ -10,6 +10,22 @@
 
     // Document ready
     $(function () {
+        // Pause audio/video/iframe when closing modal/accordion
+        $('.modal, .accordion-collapse').on('hide.bs.modal hide.bs.collapse', function () {
+            $(this)
+                .find('audio, video, iframe')
+                .each(function () {
+                    if (this.tagName == 'IFRAME') {
+                        this.contentWindow.postMessage(
+                            '{"event":"command","func":"pauseVideo","args":""}',
+                            '*'
+                        );
+                    } else {
+                        this.pause();
+                    }
+                });
+        });
+
         // Initialize Slick projects-carousel
         $('.projects-carousel').slick({
             dots: true,
