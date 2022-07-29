d98373fb3ea944372b7190afe2bf62068ff5a907
Rafa Soler <rafasoler10@gmail.com>
2022-07-29 18:26:02 +0200

    Prevent CSS & JS caching when they are modified

--- wp-content/themes/prioritat-LaTempesta/functions.php
+++ wp-content/themes/prioritat-LaTempesta/functions.php
@@ -40,9 +40,14 @@ function theme_enqueue_styles() {
 	// Define script dependencies
 	$deps = array( 'slick-script-async' );
 
-	wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) );
+	// Prevent CSS & JS caching when they are modified
+	$timestamp_styles = filemtime( get_stylesheet_directory() . $theme_styles );
+	$timestamp_scripts = filemtime( get_stylesheet_directory() . $theme_scripts );
+
+	wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) . '-' . $timestamp_styles );
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'child-understrap-scripts-async', get_stylesheet_directory_uri() . $theme_scripts, $deps, $the_theme->get( 'Version' ), true );
+	wp_enqueue_script( 'child-understrap-scripts-async', get_stylesheet_directory_uri() . $theme_scripts, $deps, $the_theme->get( 'Version' ) . '-' . $timestamp_scripts, true );
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
