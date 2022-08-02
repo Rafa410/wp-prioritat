f88f8042309af9fb2f8d259b209efbd8553990cf
Rafa Soler <rafasoler10@gmail.com>
2022-08-02 12:51:04 +0200

    Single post template

--- wp-content/themes/prioritat-LaTempesta/loop-templates/content-single-noticies.php
+++ wp-content/themes/prioritat-LaTempesta/loop-templates/content-single-noticies.php
@@ -9,7 +9,7 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class(); ?> id="noticia-<?php the_ID(); ?>">
 
 	<header class="entry-header">
 
