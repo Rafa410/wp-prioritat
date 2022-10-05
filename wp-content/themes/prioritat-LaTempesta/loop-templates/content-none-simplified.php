1e21aa88fb5d2d59d6a038c67c044d1792abcd9a
Rafa Soler <rafasoler10@gmail.com>
2022-10-05 18:04:08 +0200

    Better 404 partial template

---
+++ wp-content/themes/prioritat-LaTempesta/loop-templates/content-none-simplified.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<section class="no-results not-found">
+
+	<div class="page-content">
+
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p class="fw-light fs-5 text-muted">%s<p>',
+				esc_html__( 'No hem trobat cap entrada. Si us plau, torna a provar amb una altra cerca o categoria.', 'prioritat' )
+			);
+			
+			$kses = array( 'a' => array( 'href' => array() ) );
+			printf(
+				/* translators: 1: Link to WP admin new post page. */
+				'<p>' . wp_kses( __( 'Vols crear una entrada? <a href="%1$s">Fes click aquí</a>.', 'prioritat' ), $kses ) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+
+		elseif ( is_home() && ! current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p class="fw-light fs-5 text-muted">%s<p>',
+				esc_html__( 'No hem trobat cap entrada. Si us plau, torna a provar amb una altra cerca o categoria.', 'prioritat' )
+			);
+
+		elseif ( is_search() ) :
+
+			printf(
+				'<p class="fw-light fs-5 text-muted">%s<p>',
+				esc_html__( 'No s\'han trobat resultats, si us plau, torna a provar amb una altra cerca.', 'prioritat' )
+			);
+			get_search_form();
+
+		else :
+
+			printf(
+				'<p class="fw-light fs-5 text-muted">%s<p>',
+				esc_html__( 'No podem trobar el que estàs buscant. Potser la recerca pot ajudar.', 'prioritat' )
+			);
+			get_search_form();
+
+		endif;
+		?>
+	</div><!-- .page-content -->
+
+</section><!-- .no-results -->
