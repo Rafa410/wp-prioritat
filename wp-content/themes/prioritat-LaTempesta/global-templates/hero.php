6aeff8f56e9ec58d72e5d000614dbda2263839b4
Rafa Soler <rafasoler10@gmail.com>
2022-10-19 10:04:44 +0200

    Change order of layers

--- wp-content/themes/prioritat-LaTempesta/global-templates/hero.php
+++ wp-content/themes/prioritat-LaTempesta/global-templates/hero.php
@@ -3,6 +3,10 @@
     <div class="homepage-hero__image">
         <?php the_post_thumbnail(); ?>
     </div>
+
+    <div class="homepage-hero__pattern dot-pattern p-2 pt-5">
+        <img class="inline-svg" src="<?= get_stylesheet_directory_uri() ?>/images/dots.svg">
+    </div>
     
     <div class="container homepage-hero__content d-flex flex-column align-items-start text-light">
         <h2 class="homepage-hero__title fw-extrabold">
@@ -14,10 +18,6 @@
             data-delay="1s">
             <span><?= __( 'Vols formar-ne part?', 'prioritat' ) ?></span>
         </a>
-    </div>
-
-    <div class="homepage-hero__pattern dot-pattern p-2 pt-5">
-        <img class="inline-svg" src="<?= get_stylesheet_directory_uri() ?>/images/dots.svg">
     </div>
 
 </div>
